11a15623deec6d76e21d4ae3ddd8cfcb6588b7ab
Justin Spears <justin@augmentcode.com>
2025-10-15 20:49:35 -0700

    fix build

--- proto/index.ts
+++ proto/index.ts
@@ -1,5 +1,5 @@
-export * from './gazel_pb.js';
-export * from './logging_pb.js';
-export { type Rule, RuleSchema } from './_virtual_imports/build_proto/build_pb.js';
+export * from './generated/gazel_pb.js';
+export * from './generated/logging_pb.js';
+export { type Rule, RuleSchema } from './generated/build_pb.js';
 // Export specific types from build_pb to avoid naming conflicts
 
